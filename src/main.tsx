75f800526e959d6b99ccbeaf97d6e88b6fe4d1a2
Muneeb Ahmad <ahmmuneeb786@gmail.com>
2026-03-27 17:41:19 +0330

    downloadable

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,12 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    // Note: The '/' before sw.js is crucial because the file is in the public folder
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Sweet PWA Registered!', reg))
+      .catch(err => console.log('PWA Registration failed', err));
+  });
+}
